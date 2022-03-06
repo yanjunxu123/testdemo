55920af172f40a6f6817a617b80cd9ff508e6b8e
yjx <2509693235@qq.com>
2022-03-06 15:21:46 +0800

    Initial commit

---
+++ src/test/java/com/example/testdemo/TestdemoApplicationTests.java
@@ -0,0 +1,29 @@
+package com.example.testdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+class TestdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Autowired
+    RedisTemplate redisTemplate;
+    @Test
+    public void RedisTemplateTest() {
+
+        String key = "testTemplate:1";
+
+        this.redisTemplate.opsForValue().set(key, "testTemplatevalue1", 1000L, TimeUnit.MILLISECONDS);
+
+        String value = (String) this.redisTemplate.opsForValue().get(key);
+
+    }
+}
